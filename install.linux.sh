843059d0eed5ebbdf86cd53b1eb11483c3f55679
moljac <mcvjetko@holisticware.net>
2015-01-30 15:51:49 +0100

    scripts cleanup

--- install.linux.sh
+++
@@ -1,8 +0,0 @@
-#/bin/bash
-
-curl \
-	-sSL https://raw.githubusercontent.com/aspnet/Home/master/kvminstall.sh \
-	| \
-	sh && source ~/.kre/kvm/kvm.sh
-	
-	
--- uninstall-nuke.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-
-rmdir /S /Q %USERPROFILE%\.kre
-rmdir /S /Q %USERPROFILE%\.kpm
-
-@IF %ERRORLEVEL% NEQ 0 PAUSE	
